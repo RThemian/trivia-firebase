f7b2e3138578d4d8d9ef614b53d2e89d6378781c
RThemian <62709684+RThemian@users.noreply.github.com>
2022-11-09 08:33:58 -0700

    changes

--- src/components/algorithm.js
+++ src/components/algorithm.js
@@ -1,9 +1,27 @@
-//palindrome check function
-function palindrome(str) {
-  var re = /[\W_]/g;
-  var lowRegStr = str.toLowerCase().replace(re, "");
-  var reverseStr = lowRegStr.split("").reverse().join("");
-  return reverseStr === lowRegStr;
+//convert number to roman numeral
+function convertToRoman(num) {
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  var str = "";
+  for (var i of Object.keys(roman)) {
+    var q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+  return str;
 }
 
-console.log(palindrome("eye"));
+console.log(convertToRoman(36));
